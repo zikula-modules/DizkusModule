a0b5494c2905289bbc5b51a345ae0f3f00e4a4c9
larsneo <larsneo@zikula.org = larsneo larsneo@zikula.org>
2004-10-19 20:41:36 +0000

    forumbox based on permissions [larsneo]

---
+++ pnForum/pntemplates/plugins/function.forumbox.php
@@ -0,0 +1,52 @@
+<?php
+// $Id$
+// ----------------------------------------------------------------------
+// PostNuke Content Management System
+// Copyright (C) 2002 by the PostNuke Development Team.
+// http://www.postnuke.com/
+// ----------------------------------------------------------------------
+// Based on:
+// PHP-NUKE Web Portal System - http://phpnuke.org/
+// Thatware - http://thatware.org/
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+
+function smarty_function_forumbox($params, &$smarty) 
+{
+	extract($params); 
+	unset($params);
+
+	if(!pnModAPILoad('pnForum', 'admin')) {
+		$smarty->trigger_error("loading adminapi failed", e_error);
+	} 
+
+	$out = "";
+	$forums = pnModAPIFunc('pnForum', 'admin', 'readforums');
+
+	if(count($forums)>0) {
+		$out ='<select name="pnForum_forum[]" id="pnForum_forum[]" size="1">';
+		$out.='<option value="" selected>'. _SRCHALLTOPICS .'</option>';
+		foreach($forums as $forum) {
+			if(allowedtoreadcategoryandforum($forum['cat_id'], $forum['forum_id'])) {
+				//$out .= '<option value="' $forum[forum_id] .'">'.pnVarPrepForDisplay($forum[cat_title]).' :: '.pnVarPreForDisplay($forum[forum_name].' </option>';
+				$out .= '<option value="'.$forum[forum_id].'">'.pnVarPrepForDisplay($forum[cat_title]).'::'.pnVarPrepForDisplay($forum[forum_name]).'</option>';
+			}
+    }
+	$out .= '</select>';
+	return $out;
+}
+}
+?>
