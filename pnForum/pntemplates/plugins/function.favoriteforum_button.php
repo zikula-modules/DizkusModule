231580c0c2613a6cb4d0b66763ce1ec6f952fb9f
landseer <frank.schummertz@gmail.com = landseer frank.schummertz@gmail.com>
2005-01-04 15:11:51 +0000

    added new plugin to show add/remove favorites buttons (credits to Chris Miller aka r3ap3r)

---
+++ pnForum/pntemplates/plugins/function.favoriteforum_button.php
@@ -0,0 +1,62 @@
+<?php
+// $Id$
+// ----------------------------------------------------------------------
+// PostNuke Content Management System
+// Copyright (C) 2002 by the PostNuke Development Team.
+// http://www.postnuke.com/
+// ----------------------------------------------------------------------
+// Based on:
+// PHP-NUKE Web Portal System - http://phpnuke.org/
+// Thatware - http://thatware.org/
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+
+include_once('modules/pnForum/common.php');
+
+/**
+ * favoriteforum_button plugin
+ * adds the add to favorites forum button
+ *
+ *@params $params['forum_id'] int forum id
+ *@params $params['return_to'] string url to return to after subscribing, necessary because
+ *                                    the subscription page can be reached from several places
+ */
+function smarty_function_favoriteforum_button($params, &$smarty) 
+{
+    extract($params); 
+	unset($params);
+
+    if (pnUserLoggedIn()) {
+        $userid = pnUserGetVar('uid');
+        if(allowedtoreadcategoryandforum($cat_id, $forum_id)) {
+            if(!pnModAPILoad('pnForum', 'user')) {
+                $smarty->trigger_error("favoritesforum_button: unable to load userapi", e_error);
+                return false;
+            }
+            $lang = pnUserGetLang();
+            if(pnModAPIFunc('pnForum', 'user', 'get_forum_favorites_status',
+                            array('userid'=>$userid, 
+                                  'forum_id'=>$forum_id))==false) {
+                $out = "<a title=\"".pnVarPrepForDisplay(_PNFORUM_ADD_FAVORITE_FORUM)."\" href=\"".pnModURL('pnForum', 'user', 'prefs', array('act'=>'add_favorite_forum', 'forum'=>$forum_id, 'return_to'=>$return_to))."\"><img src=\"modules/pnForum/pnimages/$lang/add2favorites.gif\" alt=\"".pnVarPrepHTMLDisplay(_PNFORUM_ADD_FAVORITE_FORUM)."\"></a>";
+            } else {
+                $out = "<a title=\"".pnVarPrepForDisplay(_PNFORUM_REMOVE_FAVORITE_FORUM)."\" href=\"".pnModURL('pnForum', 'user', 'prefs', array('act'=>'remove_favorite_forum', 'forum'=>$forum_id, 'return_to'=>$return_to))."\"><img src=\"modules/pnForum/pnimages/$lang/removefavorite.gif\" alt=\"".pnVarPrepHTMLDisplay(_PNFORUM_REMOVE_FAVORITE_FORUM)."\"></a>";
+            }
+        }
+    }
+    return $out;
+}
+
+?>
