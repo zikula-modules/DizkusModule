9b774611fb2eae088fbf2a0315bb7c8a0ca9b070
landseer <frank.schummertz@gmail.com = landseer frank.schummertz@gmail.com>
2005-01-10 19:40:49 +0000

    *** empty log message ***

---
+++ pnForum/pntemplates/plugins/modifier.pnforumbbsmile.php
@@ -0,0 +1,68 @@
+<?php
+// $Id$
+// ----------------------------------------------------------------------
+// PostNuke Content Management System
+// Copyright (C) 2001 by the PostNuke Development Team.
+// http://www.postnuke.com/
+// ----------------------------------------------------------------------
+// Based on:
+// PHP-NUKE Web Portal System - http://phpnuke.org/
+// Thatware - http://thatware.org/
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+/**
+ * pnRender plugin
+ * 
+ * This file is a plugin for pnRender, the PostNuke implementation of Smarty
+ *
+ * @package      Xanthia_Templating_Environment
+ * @subpackage   pnRender
+ * @version      $Id$
+ * @author       The PostNuke development team
+ * @link         http://www.postnuke.com  The PostNuke Home Page
+ * @copyright    Copyright (C) 2002 by the PostNuke Development Team
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */ 
+
+ 
+/**
+ * Smarty modifier to apply the pn_bbsmile transform hooks
+ * 
+ * Available parameters:
+
+ * Example
+ * 
+ *   <!--[$MyVar|pnforumpnbbsmile]-->
+ * 
+ * 
+ * @author       Frank Schummertz
+ * @author       The pnForum team
+ * @since        16. Sept. 2003
+ * @param        array    $string     the contents to transform
+ * @return       string   the modified output
+ */
+function smarty_modifier_pnbbsmile($string)
+{
+	$extrainfo = array($string);
+
+    $return = "";
+    if(pnModAvailable('pn_bbsmile') && pnModAPILoad('pn_bbsmile', 'user')) {
+        list($return) = pnModAPIFunc('pn_bbsmile', 'user', 'transform', array('objectid' => '', 'extrainfo' => $extrainfo));
+    }
+    return $return;                      
+}
+
+?>
