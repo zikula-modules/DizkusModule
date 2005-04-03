85ed5abcfab013abc573d1d1dbfa76949fb06776
landseer <frank.schummertz@gmail.com = landseer frank.schummertz@gmail.com>
2005-04-03 09:56:43 +0000

    fixed path disclosure (thanks to jn)

--- pnForum/user/modules/editpnForum.php
+++ pnForum/user/modules/editpnForum.php
@@ -2,12 +2,17 @@
 /**
  * addon for userpage
  * @version $Id:
- * @author Andreas Krapohl 
+ * @author Andreas Krapohl
  * @copyright 2003 by Andreas Krapohl
- * @package phpBB_14 (aka pnForum) 
- * @license GPL <http://www.gnu.org/licenses/gpl.html> 
+ * @package phpBB_14 (aka pnForum)
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
  * @link http://www.pnforum.de
  */
+
+// check if we get called directly via URL, we do not like this
+if (eregi(basename(__FILE__), $_SERVER['PHP_SELF'])) {
+	die ("You can't access this file directly...");
+}
 
 /**
  * simply redirect to forum prefs
