1d0312171d0bfc185a6d3bfff2289cadb279aa98
landseer <frank.schummertz@gmail.com = landseer frank.schummertz@gmail.com>
2005-07-22 19:21:33 +0000

    new modifier to create links to user profile

---
+++ pnForum/pntemplates/plugins/modifier.profilelink.php
@@ -0,0 +1,63 @@
+<?php
+// $Id$
+// ----------------------------------------------------------------------
+// PostNuke Content Management System
+// Copyright (C) 2001 by the PostNuke Development Team.
+// http://www.postnuke.com/
+// ----------------------------------------------------------------------
+// Based on:
+// PHP-NUKE Web Portal System - http://phpnuke.org/
+// Thatware - http://thatware.org/
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+/**
+ * pnRender plugin
+ *
+ * This file is a plugin for pnRender, the PostNuke implementation of Smarty
+ *
+ * @package      Xanthia_Templating_Environment
+ * @subpackage   pnRender
+ * @version      $Id$
+ * @author       The PostNuke development team
+ * @link         http://www.postnuke.com  The PostNuke Home Page
+ * @copyright    Copyright (C) 2002 by the PostNuke Development Team
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+
+/**
+ * Smarty modifier to create a link to a users profile
+ *
+ * Available parameters:
+
+ * Example
+ *
+ *   <!--[$username|profilelink]-->
+ *
+ *
+ * @author       Frank Schummertz
+ * @author       The pnForum team
+ * @since        16. Sept. 2003
+ * @param        array    $string     the contents to transform
+ * @return       string   the modified output
+ */
+function smarty_modifier_profilelink($string)
+{
+    $string = pnVarPrepForDisplay($string);
+    return '<a title="'. pnVarPrepForDisplay(_PNFORUM_PROFILE) . ': ' . $string . '" href="user.php?op=userinfo&amp;uname=' . $string . '">' . $string . '</a>';
+}
+
+?>
