86ecabb9515a4b58e42e1861509bf9982f9b2b64
landseer <frank.schummertz@gmail.com = landseer frank.schummertz@gmail.com>
2005-08-17 14:47:36 +0000

    updated for new bbsmile javascript

--- pnForum/pntemplates/plugins/function.bbsmile.php
+++ pnForum/pntemplates/plugins/function.bbsmile.php
@@ -28,15 +28,17 @@
  * bbsmile plugin
  * shows all available smilies
  *
+ *@params textfieldid
  */
-function smarty_function_bbsmile($params, &$smarty) 
+function smarty_function_bbsmile($params, &$smarty)
 {
-    extract($params); 
+    extract($params);
 	unset($params);
 
     $out = "";
-	if(pnModAvailable('pn_bbsmile') && pnModLoad('pn_bbsmile', 'user') ) {
-	    $out = pnModFunc('pn_bbsmile', 'user', 'bbsmiles');
+	if(pnModAvailable('pn_bbsmile') &&pnModIsHooked('pn_bbsmile', 'pnForum') && pnModLoad('pn_bbsmile', 'user') ) {
+	    $out = pnModFunc('pn_bbsmile', 'user', 'bbsmiles',
+	                     array('textfieldid' => $textfieldid));
 	}
 	return $out;
 }
