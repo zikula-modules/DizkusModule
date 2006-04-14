be5f1fd21f9ad77263680a8cdfaca01d3a9df306
landseer <frank.schummertz@gmail.com = landseer frank.schummertz@gmail.com>
2006-04-14 17:49:10 +0000

    remove script tags from template using behaviour.js

--- pnForum/pnjavascript/pnforum_user_main.js
+++ pnForum/pnjavascript/pnforum_user_main.js
@@ -5,22 +5,47 @@
  *
  */
 
-function open(thiscat)
-{
-    $A(document.getElementsByClassName('category')).each(
-        function(cat)
-        {
-            if(cat.id == 'category_' + thiscat) {
-                if(Element.hasClassName(cat, 'hidden')) {
-                    Element.removeClassName(cat, 'hidden');
-                } else {
-                    Element.addClassName(cat, 'hidden');
-                }
-            } else {
-                Element.addClassName(cat, 'hidden');
-            }
+Event.observe(window, 'load', 
+    function()
+    {
+        if($A(document.getElementsByClassName('openclosecategory')).length == 0) {
+            return;
         }
-        );
-    return;
-}
+        
+        // define a rule to delete a menuitem when the trashcan icon is clicked
+        var ruleset = {
+        	'#pnf_maincategorylist .openclosecategory' : function(catlink){
+                catlink.href = 'javascript:void(0);';
+                Event.observe(catlink, 
+                              'click', 
+                              function()
+                              { 
+                                  var thiscat = this.id.split('_')[1];
+                                  $A(document.getElementsByClassName('category')).each(
+                                      function(cat)
+                                      {
+                                          if(cat.id == 'category_' + thiscat) {
+                                              if(Element.hasClassName(cat, 'hidden')) {
+                                                  Element.removeClassName(cat, 'hidden');
+                                              } else {
+                                                  Element.addClassName(cat, 'hidden');
+                                              }
+                                          } else {
+                                              Element.addClassName(cat, 'hidden');
+                                          }
+                                      }
+                                      );
+                                  return;
+                              },
+                              false);
+        	}
+        };
+        
+        // register the ruleset
+        Behaviour.register(ruleset);
+        
+        // apply the ruleset to all existing delete buttons
+        Behaviour.apply();
+    }, 
+    false);
 
