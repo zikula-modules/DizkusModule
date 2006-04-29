c556d47dc8d9f10605b8a79e6726bf0a81b5c0b1
landseer <frank.schummertz@gmail.com = landseer frank.schummertz@gmail.com>
2006-04-29 19:36:54 +0000

    added PNphpBB2 fake module

---
+++ PNphpBB2/lang/eng/global.php
@@ -0,0 +1,50 @@
+<?php
+/************************************************************************
+ * pnForum - The Post-Nuke Module                                       *
+ * ==============================                                       *
+ *                                                                      *
+ * Copyright (c) 2001-2004 by the pnForum Module Development Team       *
+ * http://www.pnforum.de/                                               *
+ ************************************************************************
+ * Modified version of:                                                 *
+ ************************************************************************
+ * phpBB version 1.4                                                    *
+ * begin                : Wed July 19 2000                              *
+ * copyright            : (C) 2001 The phpBB Group                      *
+ * email                : support@phpbb.com                             *
+ ************************************************************************
+ * License                                                              *
+ ************************************************************************
+ * This program is free software; you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation; either version 2 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program; if not, write to the Free Software          *
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 *
+ * USA                                                                  *
+ ************************************************************************
+ *
+ * english language defines
+ * @version $Id: global.php,v 1.91 2006/04/04 12:53:18 landseer Exp $
+ * @author various
+ * @copyright 2004 by pnForum team
+ * @package pnForum
+ * @license GPL <http://www.gnu.org/licenses/gpl.html>
+ * @link http://www.pnforum.de
+ *
+ ***********************************************************************/
+
+define('_PNPHPBB2_TOPICMOVEDTO', 'The requested topic has been move to');
+define('_PNPHPBB2_UPDATEBOOKMARKS', 'Please update your bookmarks');
+define('_PNPHPBB2_CLICKIFNOTREDIRECTED', 'If you do not get automatically redirected in %t% seconds, please click on the link above');
+define('_PNPHPBB2_ISNOTLONGERUSEDHERE', 'We moved our forums!');
+
+
+?>
