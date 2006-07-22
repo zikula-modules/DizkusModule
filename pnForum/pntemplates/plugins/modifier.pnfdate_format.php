c472d9b9010c7e92c93ed8a1ac3a2e6194d70425
landseer <frank.schummertz@gmail.com = landseer frank.schummertz@gmail.com>
2006-07-22 09:31:34 +0000

    fixed [#3076] Wrong time for guests

--- pnForum/pntemplates/plugins/modifier.pnfdate_format.php
+++ pnForum/pntemplates/plugins/modifier.pnfdate_format.php
@@ -52,8 +52,13 @@ require_once $smarty->_get_plugin_filepath('shared','make_timestamp');
  * @author   Steffen Voss
  * @since    15. Jan. 2004
  * @param    string   $string         input date string
- * @param    string   format          strftime format for output
+ * @param    string   format          strftime format for output, either a name that corresponds to a define
+ *                                    in the main language settings or a explicit time definition as defined
+ *                                    at http://php.net/manual/en/function.strftime.php
+ *                                    If the define used does not exist, datebrief + a red exclamation mark will
+ *                                    be shown.
  * @param    string   $default_date   default date if $string is empty
+ * @param    string   $usetzoffset    use users timezone offset if set
  * @return   string   the modified output
  * @uses     smarty_make_timestamp()
  */
@@ -62,49 +67,31 @@ function smarty_modifier_pnfdate_format($string, $format='datebrief', $default_d
     if(empty($format)) {
         $format = 'datebrief';
     }
-	switch(trim(strtolower($format))){
-		case 'datelong':
-			$format = _DATELONG;
-			break;
-		case 'datebrief':
-			$format = _DATEBRIEF;
-			break;
-		case 'datestring':
-			$format = _DATESTRING;
-			break;
-		case 'datestring2':
-			$format = _DATESTRING2;
-			break;
-		case 'datetimebrief':
-			$format = _DATETIMEBRIEF;
-			break;
-		case 'datetimelong':
-			$format = _DATETIMELONG;
-			break;
-		case 'linksdatestring':
-			$format = _LINKSDATESTRING;
-			break;
-		case 'timebrief':
-			$format = _TIMEBRIEF;
-			break;
-		case 'timelong':
-			$format = _TIMELONG;
-			break;
-		default:
-	} // switch
+
+    if(substr_count($format, '%') == 0) {
+        // format does not contain a % hence it is not a time format string but a name for a define
+        $format = '_' . strtoupper($format);
+        if(defined($format)) {
+            $format = constant($format);
+        } else {
+            // just a dumb default (datebrief + a red exclamation mark)
+            $format = _DATEBRIEF . '<span style="color: red;">!</span>';
+        }
+    }
 
     $tzoffset = 0;
     if(isset($usetzoffset)) {
-        $useroffset = (pnConfigGetVar('anonymoussessions')==1) ? (float)pnUserGetVar('timezone_offset') : (float)pnUserGetVar('timezone_offset', 1);
+        $useroffset = (pnUserLoggedIn()) ? (float)pnUserGetVar('timezone_offset') : (float)pnUserGetVar('timezone_offset', 1);
         $tzoffset = ($useroffset - (float)pnConfigGetVar('timezone_offset')) * 3600;
     }
-	if($string != '') {
-    	return strftime($format, smarty_make_timestamp($string) + $tzoffset );
-	} elseif (isset($default_date) && $default_date != '') {
-    	return strftime($format, smarty_make_timestamp($default_date) + $tzoffset );
-	} else {
-		return;
-	}
+
+    if($string != '') {
+        return strftime($format, smarty_make_timestamp($string) + $tzoffset );
+    } elseif (isset($default_date) && $default_date != '') {
+        return strftime($format, smarty_make_timestamp($default_date) + $tzoffset );
+    } else {
+        return;
+    }
 }
 
 ?>
