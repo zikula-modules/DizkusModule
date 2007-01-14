f2b44a25b2fc1f552dcd4a13dc40f042d001e42f
landseer <frank.schummertz@gmail.com = landseer frank.schummertz@gmail.com>
2007-01-14 13:04:59 +0000

    fixed color class definition with .8 when no colors are defined in the theme

--- pnForum/pntemplates/plugins/function.pnforumcolorcss.php
+++ pnForum/pntemplates/plugins/function.pnforumcolorcss.php
@@ -24,25 +24,35 @@
 // To read the license please visit http://www.gnu.org/copyleft/gpl.html
 // ----------------------------------------------------------------------
 
+include_once 'modules/pnForum/common.php';
+
 /**
  * pnforumcolorcss plugin
  * creates css styles for using colors
- * use this plugin in your themes head section!
+ * use this plugin in your themes head section if your theme generates and supports
+ * color definitions. If not, use the class defintions in the main style.css
  *
  */
 function smarty_function_pnforumcolorcss($params, &$smarty)
 {
+    // The second parameter in pnThemeGetVar() defines the default value
+    // which will be used when nothing else is defined (which might be the
+    // case for some Xanthia 3.0 themes in .8. It will be ignored in .764.
+    // If you do not like those defaults, remove the pnforumcolorcss plugin
+    // from your theme and define the colors in your pnForums style.css
+    // See modules/pnForum/pnstyle/style.css for more information about this
+    //
     $css = "\n<style type=\"text/css\">\n"
-          .".pnf_bgcolor1 { background-color: " . pnThemeGetVar('bgcolor1') . " }\n"
-          .".pnf_bgcolor2 { background-color: " . pnThemeGetVar('bgcolor2') . " }\n"
-          .".pnf_bgcolor3 { background-color: " . pnThemeGetVar('bgcolor3') . " }\n"
-          .".pnf_bgcolor4 { background-color: " . pnThemeGetVar('bgcolor4') . " }\n"
-          .".pnf_bgcolor5 { background-color: " . pnThemeGetVar('bgcolor5') . " }\n"
-          .".pnf_bordercolor1 { border-color: " . pnThemeGetVar('bgcolor1') . " }\n"
-          .".pnf_bordercolor2 { border-color: " . pnThemeGetVar('bgcolor2') . " }\n"
-          .".pnf_bordercolor3 { border-color: " . pnThemeGetVar('bgcolor3') . " }\n"
-          .".pnf_bordercolor4 { border-color: " . pnThemeGetVar('bgcolor4') . " }\n"
-          .".pnf_bordercolor5 { border-color: " . pnThemeGetVar('bgcolor5') . " }\n"
+          .".pnf_bgcolor1 { background-color: " . pnThemeGetVar('bgcolor1', '#FFFFFF') . "; }\n"
+          .".pnf_bgcolor2 { background-color: " . pnThemeGetVar('bgcolor2', '#AFBFC8') . "; }\n"
+          .".pnf_bgcolor3 { background-color: " . pnThemeGetVar('bgcolor3', '#CEDEE7') . "; }\n"
+          .".pnf_bgcolor4 { background-color: " . pnThemeGetVar('bgcolor4', '#EDF3F7') . "; }\n"
+          .".pnf_bgcolor5 { background-color: " . pnThemeGetVar('bgcolor5', '#EDF3F7') . "; }\n"
+          .".pnf_bordercolor1 { border-color: " . pnThemeGetVar('bgcolor1', '#FFFFFF') . "; }\n"
+          .".pnf_bordercolor2 { border-color: " . pnThemeGetVar('bgcolor2', '#AFBFC8') . "; }\n"
+          .".pnf_bordercolor3 { border-color: " . pnThemeGetVar('bgcolor3', '#CEDEE7') . "; }\n"
+          .".pnf_bordercolor4 { border-color: " . pnThemeGetVar('bgcolor4', '#EDF3F7') . "; }\n"
+          .".pnf_bordercolor5 { border-color: " . pnThemeGetVar('bgcolor5', '#EDF3F7') . "; }\n"
           ."</style>\n\n";
     return $css;
 }
