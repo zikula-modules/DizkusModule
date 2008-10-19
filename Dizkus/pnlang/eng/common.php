65376e2d080526ee136bed92d387665c3d0bad09
schiessl <quan.ifs@gmail.com = schiessl quan.ifs@gmail.com>
2008-10-19 18:01:25 +0000

    lang define _MODIFYCONFIG missing

--- Dizkus/pnlang/eng/common.php
+++ Dizkus/pnlang/eng/common.php
@@ -18,6 +18,7 @@ define('_DZK_NEW_THREADS','New Topic in forum');
 define('_DZK_ADD_FAVORITE_FORUM','add to favorites');
 
 // new
+define('_MODIFYCONFIG', 'Modify module settings');
 define('_DZK_POSTSAPPEARANCE', 'Posting appearance');
 define('_DZK_MANAGESIGNATURE', 'Manage my signature');
 define('_DZK_PREFS_ENABLESIGNATUREMANAGEMENT',	'Enable signature management via Dizkus-User-Settings');
--- Dizkus/pnlang/fra/common.php
+++ Dizkus/pnlang/fra/common.php
@@ -17,6 +17,7 @@
 define('_DZK_POSTSAPPEARANCE', 'Posting appearance');
 define('_DZK_MANAGESIGNATURE', 'Manage my signature');
 define('_DZK_PREFS_ENABLESIGNATUREMANAGEMENT',	'Enable signature management via Dizkus-User-Settings');
+define('_MODIFYCONFIG', 'Modify module settings');
 
 // alphasorting starts here
 
