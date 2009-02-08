9fbe8de02e82cf93b139347406d07db3372f0ace
landseer <frank.schummertz@gmail.com = landseer frank.schummertz@gmail.com>
2009-02-08 11:41:12 +0000

    test commit for codesniffer

--- Dizkus/pnjavascript/dizkus_admin_ranks.js
+++ Dizkus/pnjavascript/dizkus_admin_ranks.js
@@ -1,17 +1,17 @@
 /**
  *
- * $Id$
+ * $Id:	dizkus_admin_ranks.js 929 2008-10-25 16:14:11Z Landseer	$
  *
  */
 
-function ShowRankImage(num, path) {
-
-    $('image' + num).src = path + '/' + $F('rank_image' + num);
-    return;
+function ShowRankImage(num,	path) 
+{
+	$('image' +	num).src = path	+ '/' +	$F('rank_image'	+ num);
+	return;
 }
 
-function ShowNewRankImage(path) {
-
-    $('newimage').src = path + '/' + $F('newrank_image');
-    return;
+function ShowNewRankImage(path)	
+{
+	$('newimage').src =	path + '/' + $F('newrank_image');
+	return;
 }
