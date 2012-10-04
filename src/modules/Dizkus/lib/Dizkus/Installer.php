3c90b1364b4977bf06db9b3dd83303ad3aa8d0f9
Phaidon <dev@wuertz.org>
2012-10-04 15:07:49 +0200

    Typo

--- src/modules/Dizkus/lib/Dizkus/Installer.php
+++ src/modules/Dizkus/lib/Dizkus/Installer.php
@@ -734,7 +734,7 @@ Class Dizkus_Installer extends Zikula_AbstractInstaller
         $this->delVar('allowgravatars');
         $this->delVar('gravatarimage');
 
-        LogUtil::registerError($this->__('The permission schemas "Dizkus_Centerblock::" and "Dizkus_Statisticsblock" were changed into "Dizkus::Centerblock" and "Dizkus::Statisticsblock". If you were using them please modify your permission table.'))
+        LogUtil::registerError($this->__('The permission schemas "Dizkus_Centerblock::" and "Dizkus_Statisticsblock" were changed into "Dizkus::Centerblock" and "Dizkus::Statisticsblock". If you were using them please modify your permission table.'));
         
         return true;
     }
