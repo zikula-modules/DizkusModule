c3a128dfe5028b4596280a3699a99389fe6ccabe
Drak <drak@zikula.org>
2013-01-27 23:47:26 +0000

    Refactor online plugin to Doctrine 2

--- templates/plugins/function.dizkusonline.php
+++ templates/plugins/function.dizkusonline.php
@@ -3,14 +3,13 @@
  * Dizkus
  *
  * @copyright (c) 2001-now, Dizkus Development Team
- * @link https://github.com/zikula-modules/Dizkus
- * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
- * @package Dizkus
+ * @link      https://github.com/zikula-modules/Dizkus
+ * @license   GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package   Dizkus
  */
-                                                                                                                                                         
+
 /**
  * Renderer plugin
- *
  * This file is a plugin for Renderer, the Zikula implementation of Smarty
  *
  * @package      Xanthia_Templating_Environment
@@ -25,35 +24,33 @@
 
 /**
  * Smarty function to read the users who are online
- *
  * This function returns an array (ig assign is used) or four variables
  * numguests : number of guests online
  * numusers: number of users online
  * total: numguests + numusers
  * unames: array of 'uid', (int, userid), 'uname' (string, username) and 'admin' (boolean, true if users is a moderator)
- *
  * Available parameters:
  *   - assign:       If set, the results are assigned to the corresponding variable
  *   - checkgroups:  If set, checks if the users found are in the moderator groups (perforance issue!) default is no group check
- *
  * Example
  *   {dizkusonline assign="islogged"}
  *
- *
  * @author       Frank Chestnut
  * @since        10/10/2005
- * @param        array       $params      All attributes passed to this function from the template
- * @param        object      &$smarty     Reference to the Smarty object
+ *
+ * @param        array       $params    All attributes passed to this function from the template
+ * @param        object      &$view     Reference to the Smarty object
+ *
  * @return       array
  */
 
-function smarty_function_dizkusonline($params, &$smarty)
+function smarty_function_dizkusonline($params, Zikula_View $view)
 {
     if (!isset($params['category_id'])) {
-        $params['category_id'] = (isset($smarty->_tpl_vars['viewcat']) && $smarty->_tpl_vars['viewcat'] != -1) ? $smarty->_tpl_vars['viewcat'] : ''; 
+        $params['category_id'] = (isset($view->_tpl_vars['viewcat']) && $view->_tpl_vars['viewcat'] != -1) ? $view->_tpl_vars['viewcat'] : '';
     }
     if (!isset($params['forum_id'])) {
-        $params['forum_id'] = isset($smarty->_tpl_vars['forum']) ? $smarty->_tpl_vars['forum'] : ''; 
+        $params['forum_id'] = isset($view->_tpl_vars['forum']) ? $view->_tpl_vars['forum'] : '';
     }
 
     $params['checkgroups'] = (isset($params['checkgroups'])) ? true : false;
@@ -61,37 +58,39 @@ function smarty_function_dizkusonline($params, &$smarty)
     $ztable = DBUtil::getTables();
 
     $sessioninfocolumn = $ztable['session_info_column'];
-    $sessioninfotable  = $ztable['session_info'];
+    $sessioninfotable = $ztable['session_info'];
 
     $activetime = DateUtil::getDateTime(time() - (System::getVar('secinactivemins') * 60));
 
     // set some defaults
     $numguests = 0;
-    $numusers  = 0;
-    $unames    = array();
+    $numusers = 0;
+    $unames = array();
 
     $moderators = ModUtil::apiFunc('Dizkus', 'user', 'get_moderators', array());
 
-    if (System::getVar('anonymoussessions')) {
-        $anonwhere = "AND $ztable[session_info].uid >= '0'";
-    } else {
-        $anonwhere = "AND $ztable[session_info].uid > '0'";
-    }
-    $sql = "SELECT   $ztable[session_info].uid, $ztable[users].uname
-            FROM     $ztable[session_info], $ztable[users]
-            WHERE    $ztable[session_info].lastused > '$activetime'
-            $anonwhere
-            AND      if ($ztable[session_info].uid='0','1',$ztable[session_info].uid) = $ztable[users].uid
-            GROUP BY $ztable[session_info].ipaddr, $ztable[session_info].uid";
+    /** @var $em Doctrine\ORM\EntityManager */
+    $em = $view->getContainer()->get('doctrine.entitymanager');
 
-    $res = DBUtil::executeSQL($sql);
-    $onlineusers = DBUtil::marshallObjects($res, array('uid', 'uname'));
+    if (System::getVar('anonymoussessions')) {
+        $anonwhere = "AND s.uid >= '0'";
+    } else {
+        $anonwhere = "AND s.uid > '0'";
+    }
+    $dql = "SELECT s.uid, s.uname
+            FROM Users\Entity\UserSessionEntity s, Users\Entity\UserEntity u
+            WHERE s.lastused > '$activetime'
+            $anonwhere
+            AND      IF (s.uid='0','1',s.uid) = u.uid
+            GROUP BY s.ipaddr, s.uid";
+
+    $onlineusers = $em->createQuery($dql)->execute(null, \Doctrine\ORM\AbstractQuery::HYDRATE_ARRAY);
     if (is_array($onlineusers)) {
         $total = count($onlineusers);
         foreach ($onlineusers as $onlineuser) {
             if ($onlineuser['uid'] != 0) {
                 $params['user_id'] = $onlineuser['uid'];
-                $onlineuser['admin'] = (isset($moderators[$onlineuser['uid']]) && $moderators[$onlineuser['uid']] == $onlineuser['uname']) || ModUtil::apiFunc('Dikus', 'Permission', 'canAdministrate',$params);
+                $onlineuser['admin'] = (isset($moderators[$onlineuser['uid']]) && $moderators[$onlineuser['uid']] == $onlineuser['uname']) || ModUtil::apiFunc('Dikus', 'Permission', 'canAdministrate', $params);
                 $unames[$onlineuser['uid']] = $onlineuser;
                 $numusers++;
             } else {
@@ -104,20 +103,20 @@ function smarty_function_dizkusonline($params, &$smarty)
         foreach ($unames as $user) {
             if ($user['admin'] == false) {
                 $groups = ModUtil::apiFunc('Groups', 'user', 'getusergroups', array('uid' => $user['uid']));
-        
+
                 foreach ($groups as $group) {
-                    if (isset($moderators[$group['gid']+1000000])) {
+                    if (isset($moderators[$group['gid'] + 1000000])) {
                         $user['admin'] = true;
                     } else {
                         $user['admin'] = false;
                     }
                 }
             }
-        
-            $users[$user['uid']] = array('uid'    => $user['uid'],
-                                         'uname'  => $user['uname'],
-                                         'admin'  => $user['admin']);
-        
+
+            $users[$user['uid']] = array('uid'   => $user['uid'],
+                                         'uname' => $user['uname'],
+                                         'admin' => $user['admin']);
+
         }
         $unames = $users;
     }
@@ -125,14 +124,14 @@ function smarty_function_dizkusonline($params, &$smarty)
 
     $dizkusonline['numguests'] = $numguests;
 
-    $dizkusonline['numusers']  = $numusers;
-    $dizkusonline['total']     = $total;
-    $dizkusonline['unames']    = $unames;
+    $dizkusonline['numusers'] = $numusers;
+    $dizkusonline['total'] = $total;
+    $dizkusonline['unames'] = $unames;
 
     if (isset($params['assign'])) {
-        $smarty->assign($params['assign'], $dizkusonline);
+        $view->assign($params['assign'], $dizkusonline);
     } else {
-        $smarty->assign($dizkusonline);
+        $view->assign($dizkusonline);
     }
 
     return;
