8d3497db463ce3bb30d071b5eda44c0f788c8e9b
Craig Heydenburg <craigh@mac.com>
2013-01-31 16:26:53 -0500

    correct datecompare plugin

--- templates/plugins/function.datecompare.php
+++ templates/plugins/function.datecompare.php
@@ -19,8 +19,9 @@
 
 function smarty_function_datecompare($params, Zikula_View $view)
 {
-    $date1 = isset($params['date1']) ? (($params['date1'] instanceof DateTime) ? $params['date1'] : new DateTime("@" . strtotime($params['date1']))) : new DateTime();
-    $date2 = isset($params['date2']) ? (($params['date2'] instanceof DateTime) ? $params['date2'] : new DateTime("@" . strtotime($params['date2']))) : new DateTime();
+    /* assumes date is either DateTime Obj or unix timestamp */
+    $date1 = isset($params['date1']) ? (($params['date1'] instanceof DateTime) ? $params['date1'] : new DateTime('@' . $params['date1'])) : new DateTime();
+    $date2 = isset($params['date2']) ? (($params['date2'] instanceof DateTime) ? $params['date2'] : new DateTime('@' . $params['date2'])) : new DateTime();
     $comp = isset($params['comp']) ? $params['comp'] : "<";
 
     switch ($comp) {
@@ -41,9 +42,6 @@ function smarty_function_datecompare($params, Zikula_View $view)
             $result = ($date1 < $date2);
             break;
     }
-//    $d1f = $date1->format('Y-m-d H:i:s');
-//    $d2f = $date2->format('Y-m-d H:i:s');
-//    return $result ? "$d1f $comp $d2f true" : "$d1f $comp $d2f false";
     
     if (isset($params['assign'])) {
         $view->assign($params['assign'], $result);
