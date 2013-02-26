af0922d0eba193588f49bf45abd143a797562a70
Craig Heydenburg <craigh@mac.com>
2013-02-25 21:48:52 -0500

    remove DBUtil and fixup backforum.php. This works, but requires changes in the core's htaccess file which IMO is undesireable. Now that this is functional, it should be moved to a more appropriate location. News and PC both serve RSS feeds. I'm sure Dizkus could do the same.

--- backforum.php
+++ backforum.php
@@ -12,35 +12,23 @@
 /**
  * initialize the Zikula environment
  */
+use Zikula_Request_Http as Request;
+
 include 'lib/bootstrap.php';
+
+$request = Request::createFromGlobals();
+$core->getContainer()->set('request', $request);
 $core->init();
 
-/**
- * load Dizkus specific support functions
- */
-include_once 'modules/Dizkus/bootstrap.php';
-
-$forum_id =      FormUtil::getPassedValue('forum_id', null, 'GET');
-$cat_id   =      FormUtil::getPassedValue('cat_id', null, 'GET');
-$count    = (int)FormUtil::getPassedValue('count', 10, 'GET');
-$feed     =      FormUtil::getPassedValue('feed', 'rss20', 'GET');
-$user     =      FormUtil::getPassedValue('user', '', 'GET');
+$forum_id = $request->query->get('forum_id', null);
+$cat_id = $request->query->get('cat_id', null);
+$count = (int)$request->query->get('count', 10);
+$feed = $request->query->get('feed', 'rss20');
+$user = $request->query->get('user', null);
 
 // get the module info
 $dzkinfo = ModUtil::getInfo(ModUtil::getIdFromName('Dizkus'));
 $dzkname = $dzkinfo['displayname'];
-
-/**
- * check for feed, if not set, use rss20 as default
- */
-if (!empty($feed)) {
-    // feed is set, check counter
-    $count = (empty($count)) ? 10 : (int)$count;
-} else {
-    // set defaults
-    $feed = 'rss20';
-    $count = 10;
-}
 
 if (isset($forum_id) && !is_numeric($forum_id)) {
     die(DataUtil::formatForDisplay(__f('Error! In \'backforum.php\', an invalid forum ID %s was encountered.', $forum_id)));
@@ -50,15 +38,15 @@ if (isset($cat_id) && !is_numeric($cat_id)) {
 }
 
 /**
- * create Renderer object
+ * instanciate Zikula_View object
  */
-$render = Zikula_View::getInstance('Dizkus', false);
+$view = Zikula_View::getInstance('Dizkus', false);
 
 /**
  * check if template for feed exists
  */
 $templatefile = 'feed/' . DataUtil::formatForOS($feed) . '.tpl';
-if (!$render->template_exists($templatefile)) {
+if (!$view->template_exists($templatefile)) {
     // silently stop working
     die(DataUtil::formatForDisplay(__f('Error! Could not find a template for an %s-type feed.', $feed)));
 }
@@ -77,127 +65,91 @@ if (!empty($user)) {
 $link = ModUtil::url('Dizkus', 'user', 'main', array(), null, null, true);
 
 $forumname = DataUtil::formatForDisplay($dzkname);
+
 // default where clause => no where clause
-$where = '';
+$where = array();
 
 /**
  * check for forum_id
  */
 if (!empty($forum_id)) {
-    // TODO 'readuserforums' has been deleted! try new Dizkus_Manager_Forum($id)
-    $forum = ModUtil::apiFunc('Dizkus', 'user', 'readuserforums', array('forum_id' => $forum_id));
-    if (count($forum) == 0) {
-        // not allowed to see forum
+    $managedForum = new Dizkus_Manager_Forum($forum_id);
+    if (!SecurityUtil::checkPermission('Dizkus::', ":$forum_id:", ACCESS_READ)) {
         System::shutDown();
     }
-    $where = 'AND t.forum_id = ' . (int)DataUtil::formatForStore($forum_id) . ' ';
+    $where = array('t.forum', (int)DataUtil::formatForStore($forum_id), '=');
     $link = ModUtil::url('Dizkus', 'user', 'viewforum', array('forum' => $forum_id), null, null, true);
-    $forumname = $forum['forum_name'];
-
+    $forumname = $managedForum->get()->getForum_name();
 } elseif (!empty($cat_id)) {
+    $managedForum = new Dizkus_Manager_Forum($cat_id);
     if (!SecurityUtil::checkPermission('Dizkus::', $cat_id . ':.*:', ACCESS_READ)) {
         System::shutDown();
     }
-    $category = ModUtil::apiFunc('Dizkus', 'admin', 'readcategories',
-                             array('cat_id' => $cat_id));
-    if ($category == false) {
-        System::shutDown();
-    }
-    $where = 'AND f.cat_id = ' . (int)DataUtil::formatForStore($cat_id) . ' ';
-    $link = ModUtil::url('Dizkus', 'user', 'main', array('viewcat' => $cat_id), null, null, true);
-    $forumname = $category['cat_title'];
-
+    $where = array('t.parent', (int)DataUtil::formatForStore($cat_id), '=');
+    $link = ModUtil::url('Dizkus', 'user', 'viewforum', array('viewcat' => $cat_id), null, null, true);
+    $forumname = $managedForum->get()->getParent()->getForum_name();
 } elseif (isset($uid) && ($uid<>false)) {
-    $where = 'AND p.poster_id=' . $uid . ' ';
+    $where = array('p.poster', ' $uid', '=');
 } else {
-    // TODO 'readuserforums' has been deleted! try new Dizkus_Manager_Forum($id)
-    $userforums = ModUtil::apiFunc('Dizkus', 'user', 'readuserforums');
-    // now create a very simple array of forum_ids only. we do not need
-    // all the other stuff in the $userforums array entries
-    $allowedforums = array_map('_get_forum_ids', $userforums);
+    $allowedforums = ModUtil::apiFunc('Dizkus', 'forum', 'getForumIdsByPermission');
     if (count($allowedforums) > 0) {
-        $where = ' AND f.forum_id IN (' . DataUtil::formatForStore(implode(',', $allowedforums)) . ') ';
+        $where = array('f.forum', DataUtil::formatForStore($allowedforums), 'IN');
     }
 }
 
-/**
- * helper function to extract forum_ids from forum array
- */
-function _get_forum_ids($f)
-{
-    return $f['forum_id'];
+$view->assign('forum_name', $forumname);
+$view->assign('forum_link', $link);
+$view->assign('sitename', System::getVar('sitename'));
+$view->assign('adminmail', System::getVar('adminmail'));
+$view->assign('current_date', date(DATE_RSS));
+$view->assign('current_language', ZLanguage::getLocale());
+
+/* @var $_em \Doctrine\ORM\EntityManager */
+$_em = ServiceUtil::getService('doctrine.entitymanager');
+$qb = $_em->createQueryBuilder();
+$qb->select('t, f, p, fu')
+        ->from('Dizkus_Entity_Topic', 't')
+        ->join('t.forum', 'f')
+        ->join('t.last_post', 'p')
+        ->join('p.poster', 'fu');
+if (!empty($where)) {
+    if ($where[2] == 'IN') {
+        $qb->expr()->in($where[0], $where[1]);
+    } else {
+        $qb->where("$where[0] $where[2] :param")
+                ->setParameter('param', $where[1]);
+    }
 }
-
-$render->assign('forum_name', $forumname);
-$render->assign('forum_link', $link);
-$render->assign('sitename', System::getVar('sitename'));
-$render->assign('adminmail', System::getVar('adminmail'));
-$render->assign('current_date', date(DATE_RSS));
-$render->assign('current_language', ZLanguage::getLocale());
-
-/**
- * get database information
- */
-ModUtil::dbInfoLoad('Dizkus');
-$ztable = DBUtil::getTables();
-
-/**
- * SQL statement to fetch last 10 topics
- */
-$topicscols = DBUtil::_getAllColumnsQualified('dizkus_topics', 't');
-$sql = 'SELECT '.$topicscols.',
-                 f.forum_name,
-                 p.poster_id,
-                 p.post_time,
-                 c.cat_id,
-                 c.cat_title
-        FROM '.$ztable['dizkus_topics'].' as t,
-             '.$ztable['dizkus_forums'].' as f,
-             '.$ztable['dizkus_posts'].' as p,
-             '.$ztable['dizkus_categories'].' as c
-        WHERE t.forum_id = f.forum_id AND
-              t.topic_last_post_id = p.post_id AND
-              f.cat_id = c.cat_id
-             '.$where.'
-        ORDER BY p.post_time DESC
-        LIMIT ' . DataUtil::formatForStore($count);
+$qb->orderBy('t.topic_time', 'DESC')
+        ->setMaxResults($count);
+$topics = $qb->getQuery()->getResult();
 
 $posts_per_page  = ModUtil::getVar('Dizkus', 'posts_per_page');
-
-$res = DBUtil::executeSQL($sql);
-
-$colarray = DBUtil::getColumnsArray ('dizkus_topics');
-$colarray[] = 'forum_name';
-$colarray[] = 'poster_id';
-$colarray[] = 'post_time';
-$colarray[] = 'cat_id';
-$colarray[] = 'cat_title';
-
-$posts = DBUtil::marshallObjects($res, $colarray);
-
-$keys = array_keys($posts);
-foreach ($keys as $key)
+$posts = array();
+$i = 0;
+foreach ($topics as $topic)
 {
-    $posts[$key]['time'] = $posts[$key]['post_time'];
-    $posts[$key]['unixtime'] = strtotime ($posts[$key]['post_time']);
-    $start = (int)((ceil(($posts[$key]['topic_replies'] + 1)  / $posts_per_page) - 1) * $posts_per_page);
-
-    $posts[$key]['post_url'] = ModUtil::url('Dizkus', 'user', 'viewtopic',
-                                 array('topic' => $posts[$key]['topic_id'],
+    /* @var $topic Dizkus_Entity_Topic */
+    $posts[$i]['topic_title'] = $topic->getTopic_title();
+    $posts[$i]['cat_title'] = $topic->getForum()->getParent()->getForum_name();
+    $posts[$i]['forum_name'] = $topic->getForum()->getForum_name();
+    $posts[$i]['time'] = $topic->getTopic_time();
+    $posts[$i]['unixtime'] = $topic->getTopic_time()->format('U');
+    $start = (int)((ceil(($topic->getTopic_replies() + 1)  / $posts_per_page) - 1) * $posts_per_page);
+    $posts[$i]['post_url'] = ModUtil::url('Dizkus', 'user', 'viewtopic',
+                                 array('topic' => $topic->getTopic_id(),
                                        'start' => $start), 
                                  null, null, true);
-
-    $posts[$key]['last_post_url'] = ModUtil::url('Dizkus', 'user', 'viewtopic',
-                                      array('topic' => $posts[$key]['topic_id'],
+    $posts[$i]['last_post_url'] = ModUtil::url('Dizkus', 'user', 'viewtopic',
+                                      array('topic' => $topic->getTopic_id(),
                                             'start' => $start), 
-                                      null, "pid" . $posts[$key]['topic_last_post_id'], true);
-
-    //$posts[$key]['rsstime'] = strftime('%a, %d %b %Y %H:%M:%S %Z', $posts[$key]['post_unixtime']);
-    $posts[$key]['rsstime'] = date(DATE_RSS, $posts[$key]['unixtime']);
+                                      null, "pid" . $topic->getLast_post()->getPost_id(), true);
+    $posts[$i]['rsstime'] = $topic->getTopic_time()->format(DATE_RSS);
+    $i++;
 }
 
-$render->assign('posts', $posts);
-$render->assign('dizkusinfo', $dzkinfo);
+$view->assign('posts', $posts);
+$view->assign('dizkusinfo', $dzkinfo);
 
 header("Content-Type: text/xml");
-$render->display($templatefile);
+$view->display($templatefile);
