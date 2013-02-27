402326a520aacde461afbb08a480b39cf448a9f9
Craig Heydenburg <craigh@mac.com>
2013-02-26 19:09:06 -0500

    remove  unused debug function

--- bootstrap.php
+++ bootstrap.php
@@ -14,34 +14,3 @@ $helper->getListener('standardfields');
 
 $em = ServiceUtil::getService('doctrine.entitymanager');
 $em->getEventManager()->addEventSubscriber(new \Gedmo\Tree\TreeListener());
-
-/**
- * internal debug function
- *
- */
-function dzkdebug($name='', $data, $die = false)
-{
-    if (SecurityUtil::checkPermission('Dizkus::', '::', ACCESS_ADMIN)) {
-        $type = gettype($data);
-        echo "\n<!-- begin debug of $name -->\n<div style=\"color: red;\">$name ($type";
-        if (is_array($data)||is_object($data)) {
-            if (count($data) > 0) {
-                echo ', size=$size entries):<pre>';
-                echo htmlspecialchars(print_r($data, true));
-                echo '</pre>:<br />';
-            } else {
-                echo '):empty<br />';
-            }
-        } else if (is_bool($data)) {
-            echo ($data==true) ? ") true<br />" : ") false<br />";
-        } else if (is_string($data)) {
-            echo ', len='.strlen($data).') :'.DataUil::formatForDisplay($data).':<br />';
-        } else {
-            echo ') :'.$data.':<br />';
-        }
-        echo '</div><br />\n<!-- end debug of $name -->';
-        if ($die==true) {
-            System::shutDown();
-        }
-    }
-}
