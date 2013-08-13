92ee2099c0b24fcd345fe722f23c2adacf968772
Craig Heydenburg <craigh@mac.com>
2013-08-13 11:10:52 -0400

    rework admin ranks to be jquery based. refs #156, #92

---
+++ javascript/Zikula.Dizkus.Admin.Ranks.js
@@ -0,0 +1,15 @@
+/**
+ * Zikula.Dizkus.Admin.Ranks.js
+ *
+ * jQuery based JS
+ */
+
+jQuery(document).ready(function () {
+    var rankImagePath = jQuery('#rankImagesPath').val();
+    jQuery('#newrank_image').change(function() {
+    	jQuery('#newimage').attr('src', rankImagePath + '/' + jQuery(this).val());
+    });
+    jQuery('.rankimageselect').change(function() {
+        jQuery(this).next("img").attr('src', rankImagePath + '/' + jQuery(this).val());
+    });
+});
