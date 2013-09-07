a05c586cea6fca384ec04f2056c8eb20cdbee77d
Craig Heydenburg <craigh@mac.com>
2013-09-07 09:59:56 -0400

    correct ajax.php calls for module name

--- Zikula/Module/DizkusModule/Resources/public/js/Zikula.Dizkus.Admin.ManageSubscriptions.js
+++ Zikula/Module/DizkusModule/Resources/public/js/Zikula.Dizkus.Admin.ManageSubscriptions.js
@@ -6,7 +6,7 @@
 
 jQuery(document).ready(function() {
     jQuery('#username').autocomplete({
-        serviceUrl: Zikula.Config.baseURL + "ajax.php?module=Dizkus&type=ajax&func=getUsers",
+        serviceUrl: Zikula.Config.baseURL + "ajax.php?module=ZikulaDizkusModule&type=ajax&func=getUsers",
         paramName: 'fragment',
         onSelect: function(suggestion) {
             console.log(suggestion);
