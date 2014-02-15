4f0fd9b61d8f031eb43ddcf1ea8d528a20320aca
Craig Heydenburg <craigh@mac.com>
2014-02-15 09:20:29 -0500

    raise core_min

--- Zikula/Module/DizkusModule/DizkusModuleVersion.php
+++ Zikula/Module/DizkusModule/DizkusModuleVersion.php
@@ -36,7 +36,7 @@ class DizkusModuleVersion extends \Zikula_AbstractVersion
         $meta['description'] = $this->__('An integrated discussion forum for Zikula.');
         $meta['url'] = $this->__('forums');
         $meta['version'] = '4.0.0';
-        $meta['core_min'] = '1.3.7';
+        $meta['core_min'] = '1.4.0';
         // $meta['core_max'] = '1.3.99';
         $meta['securityschema'] = array(
             'Dizkus::' => 'ForumID::',
