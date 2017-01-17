52cfca5d864b7000f63540bb940a268f9356133a
Kaik <kapitanogor@googlemail.com>
2017-01-17 20:52:29 +0100

    style check 4

--- Block/RecentPostsBlock.php
+++ Block/RecentPostsBlock.php
@@ -6,7 +6,7 @@
  * @copyright (c) 2001-now, Dizkus Development Team
  *
  * @link https://github.com/zikula-modules/Dizkus
- * 
+ *
  * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
  */
 
