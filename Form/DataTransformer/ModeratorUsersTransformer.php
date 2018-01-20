6386c3baa1c869e024d840c06084b2f35945684f
Kaik <kapitanogor@googlemail.com>
2018-01-20 22:45:10 +0100

    Modify forum fixes - update chosen to 1.8.2 - remove old jQuery Autocomplete - convert modify forum js to module and namespace - add ajax user search for user moderators

--- Form/DataTransformer/ModeratorUsersTransformer.php
+++ Form/DataTransformer/ModeratorUsersTransformer.php
@@ -45,7 +45,11 @@ class ModeratorUsersTransformer implements DataTransformerInterface
 
         $selectedUsersArr = [];
         foreach ($moderatorUsersCollection as $element) {
-            $selectedUsersArr[$element->getForumUser()->getUserId()] = $element->getForumUser()->getUserId();
+            $uid = $element->getForumUser()->getUser() === null ? null : $element->getForumUser()->getUser()->getUid();
+            if (!$uid) {
+                continue;
+            }
+            $selectedUsersArr[$element->getForumUser()->getUser()->getUname()] = $uid;
         }
 
         return $selectedUsersArr;
